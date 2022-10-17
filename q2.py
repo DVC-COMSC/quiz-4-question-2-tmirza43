1a3a22cf6cfb60973489250c1317d7b302de4f65
Talha Mirza <tmirza43@gmail.com>
2022-10-16 18:14:44 -0700

    commit1

--- q2.py
+++ q2.py
@@ -8,3 +8,24 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+string = ''
+longest = ''
+shortest = ''
+prev = ''
+count = 0
+
+while string != "stop" or string != "STOP":
+  string = str(input("Enter string: "))
+  if string == "stop" or string == "STOP":
+    break
+  if len(string) > len(longest):
+    longest = string
+  if count == 0:
+    shortest = string
+  if len(string) < len(shortest):
+    shortest = string
+  count +=1
+
+print(f"Shortest: {shortest}")
+print(f"Longest: {longest}")
